cac8918a6941c7e0abf9d371d0eeaa69e541c0b1
Saad <educationalmindz@gmail.com>
2024-05-27 18:32:22 -0400

    Styled project with SCSS

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const pianoKeys = document.querySelectorAll(".piano-keys .key")
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
 let audio = new Audio("tunes/a.wav"); // by default, audio src is a tune
 
 const playTune = (key) => {
-    audio.src = 'tunes/${key}.wav';
-    audio.play(); // playing audio
+  audio.src = `tunes/${key}.wav`; // use backticks for string interpolation
+  audio.play(); // playing audio
+};
 
-}
-
-pianoKeys.forEach(key => {
-   // calling playTune function with passing data-key value as an argument
-    key.addEventListener("click", () =>playTune(key.dataset.key));
-}); 
+pianoKeys.forEach((key) => {
+  // calling playTune function with passing data-key value as an argument
+  key.addEventListener("click", () => playTune(key.dataset.key));
+});
